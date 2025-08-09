fa7481734ca4fb914a9f038e859120f2c75c24ce
Leonardo Sudré <165829938+Ls-Leonardo@users.noreply.github.com>
2025-08-09 14:47:44 -0300

    Initial project setup: Password Generator
    - Initial project structure with HTML, CSS, and JavaScript.
    - Password generator implementation with length validation and copy option.
    - Added smooth animations to the modal.
    - Responsive layout optimized for mobile and desktop.

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# gerador-senhas
+# Gerador de Senhas Aleatórias
+
+## Descrição
+
+Este é um gerador de senhas simples e eficiente, feito com **HTML**, **CSS** e **JavaScript**. Ele permite que o usuário gere senhas seguras, configurando o tamanho da senha e copiando-a para a área de transferência com um único clique.
+
+O projeto utiliza animações suaves para uma experiência mais agradável e é totalmente responsivo, funcionando bem em dispositivos móveis e desktops.
+
+
+## Funcionalidades
+
+- Geração de senhas aleatórias com caracteres especiais, letras maiúsculas e minúsculas, e números.
+- Opção de copiar a senha gerada para a área de transferência.
+- Animações suaves ao gerar e copiar a senha.
+- Layout responsivo, ideal para dispositivos móveis e desktop.
+
+## Link para Acessar
+
+Você pode acessar o projeto [aqui](https://devls-io.github.io/gerador-senhas/).
+
+## Tecnologias Usadas
+
+- **HTML5** para a estrutura do conteúdo.
+- **CSS3** para o design e animações.
+- **JavaScript** para a lógica de geração de senhas e interação com o usuário.
+
+
+
+## Licença
+
+Este projeto está licenciado sob a MIT License - veja o arquivo [LICENSE](LICENSE) para mais detalhes.
+
---
+++ script.js
@@ -0,0 +1,118 @@
+class PasswordGenerator{
+    constructor(){
+        this.caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*-_,.~";
+    }
+
+    generate(length){
+        if(!length || isNaN(length) || length < 1 || length > 50){
+            throw new Error("Por favor, insira um número válido para o tamanho da senha. O tamanho deve ser entre 1 e 50.");
+
+        }
+
+         let password = ""
+
+         for(let i = 0; i < length; i++){
+
+           
+            const randomIndex = Math.floor(Math.random() * this.caracteres.length)
+            password += this.caracteres[randomIndex]
+         }
+
+         return password
+    }
+}
+
+class ClipboardManager{
+    static copyToClipboard(text){
+        return navigator.clipboard.writeText(text)
+    }
+}
+
+class Modal{
+    constructor(){
+        this.modal = document.getElementById("modal")
+        this.modalMessage = document.getElementById("modal-message")
+        this.btnClose = document.getElementById("modal-close")
+
+        this.btnClose.addEventListener("click", ()=> this.close());
+
+        window.addEventListener("click", (event)=> {
+            if(event.target === this.modal){
+                this.close()
+            }
+        })
+
+    }
+
+    open(message){
+            this.modal.style.display = "flex"
+            this.modal.classList.add("show")
+            this.modalMessage.textContent = message
+    }
+
+    close(){
+        this.modal.classList.remove("show")
+
+        setTimeout(()=> {
+            this.modal.style.display = "none"
+        }, 300)
+       
+    }
+}
+
+
+class App{
+    constructor(){
+        this.passwordGenerator = new PasswordGenerator();
+        this.modal = new Modal()
+
+        this.inputLength = document.getElementById("pass-length")
+        this.passwordDisplay = document.getElementById("password-display")
+        this.formGenerate = document.getElementById("formGenerate")
+        this.btnCopy = document.getElementById("copy-pass")
+
+        this.formGenerate.addEventListener("submit", (e)=> {
+            e.preventDefault() // interromper o envio
+            this.generatePassword() // gerar a senha
+        })
+
+        this.btnCopy.addEventListener("click", ()=> this.copyPassword())
+    }
+
+    generatePassword(){
+        try{
+            const length = parseInt(this.inputLength.value.trim(), 10) // string para decimal b10
+
+            const password = this.passwordGenerator.generate(length)
+
+            this.passwordDisplay.textContent = password
+            this.inputLength.value = ""
+            this.btnCopy.style.display = "block"
+            this.modal.open("Senha gerada com sucesso!")
+
+        } catch(error){
+            this.modal.open(error.message)
+        }
+    }
+
+    copyPassword(){
+        const password = this.passwordDisplay.textContent
+        if(password){
+            ClipboardManager.copyToClipboard(password)
+            .then(() => this.modal.open("Senha copiada!"))
+            .catch(() => this.modal.open("Erro ao copiar a senha!"));
+        } else{
+            this.modal.open("Não há uma senha para copiar!")
+        }
+    }
+
+    
+}
+
+
+// Inicializando nosso APP
+
+document.addEventListener("DOMContentLoaded", ()=> new App())
+
+
+
